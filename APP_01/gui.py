007fc5fee13bec55dea3674103f043ffd17cfbfb
Th3Deep <128376733+Th3Deep@users.noreply.github.com>
2023-07-01 04:56:33 -0300

    Change theme, add icons and handle error when list_box empty

--- APP_01/gui.py
+++ APP_01/gui.py
@@ -2,29 +2,28 @@ import functions
 import PySimpleGUI as sg
 import time
 
-sg.theme("DarkPurple1")
+sg.theme("Black")
 # sg.theme_previewer() [it shows all themes you can apply]
-
 
 clock = sg.Text('', key="Clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(image_source="add.png", key="Add", mouseover_colors="LightBlue",
+                       tooltip="Add todo")
 
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 
-edit_button = sg.Button("Edit")
-
-complete_button = sg.Button("Complete")
-
-exit_button = sg.Button('Exit')
+edit_button = sg.Button(size=1, image_source="edit.png", key="Edit")
+complete_button = sg.Button(image_source="complete.png", key="Complete")
+exit_button = sg.Button(image_source="off.png", key='Exit')
 
 window = sg.Window("My To-Do App",
                    layout=[[clock],
                            [label],
                            [input_box, add_button],
-                           [list_box, edit_button, complete_button],
+                           [edit_button, complete_button],
+                           [list_box],
                            [exit_button]],
                    font=('Helvetica', 12))
 
@@ -67,7 +66,10 @@ while True:
             except IndexError:
                 sg.popup("Select an item first", font=("Helvetica", 12))
         case "todos":
-            window['todo'].update(value=values['todos'][0])
+            try:
+                window['todo'].update(value=values['todos'][0])
+            except IndexError:
+                sg.popup("The list has no items, try adding one", font=("Helvetica", 12))
 
         case 'Exit':
             sg.popup_yes_no("You want to leave?")
